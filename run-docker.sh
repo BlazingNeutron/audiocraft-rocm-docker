b98bbd6a17ee5d71805f7e674bc3ff68c4fdebc8
BlazingNeutron <steven.armstrong+github@gmail.com>
2024-03-01 16:09:06 -0500

    corrected model cache folder

--- run-docker.sh
+++ run-docker.sh
@@ -2,5 +2,5 @@
 mkdir -p models
 docker run -it --network=host --device=/dev/kfd --device=/dev/dri --group-add=video \
     --ipc=host --cap-add=SYS_PTRACE --security-opt seccomp=unconfined \
-    -v ./models:/audiocraft/.cache \
+    -v ./models:/opt/audiocraft/.cache \
     audiocraftdocker:latest
